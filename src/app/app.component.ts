0f680ed8a324b7db86b95d6f8ed576b66273b05c
Will Reichl <willreichl@gmail.com>
2019-09-03 07:11:09 -0400

    S18: Showing a loading indicator while fetching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -37,6 +38,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-48c3b.firebaseio.com/posts.json'
@@ -53,6 +55,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
