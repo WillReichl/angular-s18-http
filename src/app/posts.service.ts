982eabd81e8c3f53e902143248fcb8ebb67adaf1
Will Reichl <willreichl@gmail.com>
2019-09-05 07:21:31 -0400

    S18: Observing different types of Http responses, and using 'tap' method

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpEventType
+} from '@angular/common/http';
 import { Post } from './post.model';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
 @Injectable({
@@ -20,7 +25,10 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://ng-complete-guide-48c3b.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
       )
       .subscribe(
         responseData => {
@@ -67,8 +75,20 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete(
-      'https://ng-complete-guide-48c3b.firebaseio.com/posts.json'
-    );
+    return this.http
+      .delete('https://ng-complete-guide-48c3b.firebaseio.com/posts.json', {
+        observe: 'events'
+      })
+      .pipe(
+        tap(event => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            console.log('Delete request was sent');
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
