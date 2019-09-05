6a9c0a4938e72c5ebbe032c92c15c577a34ffecb
Will Reichl <willreichl@gmail.com>
2019-09-05 06:54:41 -0400

    S18: Setting custom headers on request

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Post } from './post.model';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
@@ -35,7 +35,12 @@ export class PostsService {
   fetchPosts() {
     return this.http
       .get<{ [key: string]: Post }>(
-        'https://ng-complete-guide-48c3b.firebaseio.com/posts.json'
+        'https://ng-complete-guide-48c3b.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({
+            'Custom-Header': 'Hello'
+          })
+        }
       )
       .pipe(
         map((responseData: { [key: string]: Post }) => {
