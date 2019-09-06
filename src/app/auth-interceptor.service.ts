c1dd8284b79bcef5d328f2152c3e25522fa3cba9
Will Reichl <willreichl@gmail.com>
2019-09-06 07:47:40 -0400

    S18: Multiple interceptors -- order in app.module.ts dictates order interceptors run

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -11,23 +11,10 @@ import { tap } from 'rxjs/internal/operators/tap';
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
-  // intercept(req: import("@angular/common/http").HttpRequest<any>, next: import("@angular/common/http").HttpHandler): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
-  //   throw new Error("Method not implemented.");
-  // }
-
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log('Request is on its way');
     const modifiedRequest = req.clone({
       headers: req.headers.append('Auth', 'xyz')
     });
-    return next.handle(modifiedRequest).pipe(
-      tap(event => {
-        console.log(event);
-        if (event.type === HttpEventType.Response) {
-          console.log('Response arrived, body data: ');
-          console.log(event.body);
-        }
-      })
-    );
+    return next.handle(modifiedRequest);
   }
 }
