88b2956fb5a7b7e334d527071ed866ab3372a3aa
Will Reichl <willreichl@gmail.com>
2019-09-06 06:50:24 -0400

    S18: Introducing Interceptors

---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler
+} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  // intercept(req: import("@angular/common/http").HttpRequest<any>, next: import("@angular/common/http").HttpHandler): import("rxjs").Observable<import("@angular/common/http").HttpEvent<any>> {
+  //   throw new Error("Method not implemented.");
+  // }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    console.log('Request is on its way');
+    return next.handle(req);
+  }
+}
